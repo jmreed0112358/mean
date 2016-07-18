2ef76861acc711df64000b43b50dc22c8cd35aff
Jeremy M. Reed <reeje76@gmail.com>
2016-07-18 16:22:57 -0600

    Update test output.
    Print out current node version.

--- config/lib/app.js
+++ config/lib/app.js
@@ -43,6 +43,7 @@ module.exports.start = function start(callback) {
       // Logging initialization
       console.log('--');
       console.log(chalk.green(config.app.title));
+      console.log(chalk.green('Node version:\t\t\t' + process.version));
       console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
       console.log(chalk.green('Port:\t\t\t\t' + config.port));
       console.log(chalk.green('Database:\t\t\t\t' + config.db.uri));
